667b29767c9c81a8b74ec103bb154a611c79efc1
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-02 00:07:32 -0400

    passing tests after refactor

---
+++ index.js
@@ -0,0 +1,15 @@
+var inside = require('turf-inside')
+var featureCollection = require('turf-featurecollection')
+
+module.exports = function(ptFC, polyFC){
+  pointsWithin = featureCollection([])
+  polyFC.features.forEach(function(poly){
+    ptFC.features.forEach(function(pt){
+      var isInside = inside(pt, poly)
+      if(isInside){
+        pointsWithin.features.push(pt)
+      }
+    })
+  })
+  return pointsWithin
+}
